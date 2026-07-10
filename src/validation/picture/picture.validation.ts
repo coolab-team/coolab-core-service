82f28f143a67c8e7b926d6c00ce55224c5f00525
Emiliano Oliveira <emilianom.oliveira.n@gmail.com>
2026-07-09 22:14:33 -0300

    Remove OpenAPI metadata from picture validation

--- src/validation/picture/picture.validation.ts
+++ src/validation/picture/picture.validation.ts
@@ -3,8 +3,4 @@ import { z } from 'zod';
 export const picture = () => z.string()
   .refine(value => value.endsWith('.png'), {
     message: 'Picture must be a PNG path.',
-  })
-  .openapi({
-    description: 'A PNG picture path.',
-    type: 'string',
   });
